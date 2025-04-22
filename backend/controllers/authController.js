b7389e9e438163656f3a498cb3b5b3c5ff7dfd71
Gunabala Dilaksan <166600705+DilaksanGunabala@users.noreply.github.com>
2025-04-22 15:08:05 +0530

    Add files via upload

---
+++ backend/controllers/authController.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully", email });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "Invalid email or password" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid email or password" });
+
+    res.status(200).json({ message: "Login successful", email });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
